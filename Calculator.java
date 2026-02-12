fa4567e9835c93a47e4bab6e05b6b82833953655
junnnhhh <imk1017@naver.com>
2026-02-12 15:52:22 +0900

    doAdd() 수정

--- Calculator.java
+++ Calculator.java
@@ -1,10 +1,14 @@
 public class Calculator {
 
     public int add(int a, int b) {
-        return a + b;
+        return doAdd(a, b);
     }
-    // 추가 (E)
+    // 추가(E)
+    // 수정 (F)
     private int doAdd(int a, int b) {
+        if (a < 0 || b < 0) {
+            throw new IllegalArgumentException("negative not allowed");
+        }
         return a + b;
     }
 }
